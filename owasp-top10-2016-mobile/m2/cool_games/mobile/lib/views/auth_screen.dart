5a258d64e913c3a4d926277048b2d3e4016fb81f
Gustavo Covas <gtcovas@gmail.com>
2020-05-06 11:02:57 -0300

    Obscure text in password field

--- owasp-top10-2016-mobile/m2/cool_games/mobile/lib/views/auth_screen.dart
+++ owasp-top10-2016-mobile/m2/cool_games/mobile/lib/views/auth_screen.dart
@@ -122,6 +122,7 @@ class _AuthScreenState extends State<AuthScreen> {
                             color: Colors.black,
                           ),
                         ),
+                        obscureText: true,
                         controller: passwordController,
                       ),
                     ),
